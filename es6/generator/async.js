3595843522d0f807d5d0c1dc7ff717b7f21c1909
lemon <1041707577@qq.com>
2022-05-31 23:25:12 +0800

    feat: generator

--- es6/generator/async.js
+++ es6/generator/async.js
@@ -1,45 +1,60 @@
 //#region async
 function req() {
-    return () => new Promise((resolve) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             resolve('foo')
         }, 1000)
+    })
+}
 
+function req2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('bar')
+        }, 1000)
     })
 }
 
 async function run() {
-    const _req = req()
-    const res = await _req()
+    const res = await req()
     console.log(res);//foo
+    const res2 = await req2()
+    console.log(res2); // 'bar'
 }
 
 // run()
 //#endregion async
 
-function m_async() {
-    const _req = req()
-    const res = runGenerator(_req())
+//#region m_async
+function* m_run() {
+    const res = yield req()
     console.log(res);
+    const res2 = yield req2()
+    console.log(res2);
 }
 
-function* generator() {
-    const _req = req()
-    const num = yield _req()
-    yield num
+/**
+ * @param {Generator} g 
+ */
+function m_async(g) {
+    const it = g 
+    const { value: p, done } = it.next() // 执行到第一条yield语句，value = req() = Promise对象 
+
+    runner(p, done) // 递归调用it.next执行后续的yield语句
+
+    function runner(p, done) {
+        if (!done) {
+            p.then(res => { // 用Promise.then获取结果
+                const { value: p, done } = it.next(res) // 将结果作为yield的返回值传回生成器，并执行到下一条yield语句，获取到新的Promise对象 req2()
+                runner(p, done) //重复这个过程
+            })
+        }
+    }
 }
 
-function runGenerator(g) {
-    const it = g()
-    const { value: p } = it.next() // {value : Promise , done : false}
+m_async(m_run())
 
-    p.then(res => {
-        const { value: num } = it.next(res)
-        console.log(num);
-    })
-}
-
-// runGenerator(generator)
+//#endregion m_async
 
 
 
--- es6/generator/prime.js
+++ es6/generator/prime.js
@@ -1,37 +1,34 @@
-function* list() {
+function* list() { // 初始序列
     let num = 2
     while (true) {
         yield num++
     }
 }
 
-
-function filterFunc(n) {
+function filterFunc(n) { // 筛选条件，判断目标值能否被整除
     return (x) => x % n !== 0
 }
 
-function m_filter(fn, arr) {
-    return function*() { // 返回新的生成器
-        for (const item of arr) { // 遍历旧的生成器，调用next()时返回符合筛选函数的结果
-            if (fn(item)) {
-                yield item
-            }
+function* m_filter(fn, arr) {
+    for (const item of arr) { // 遍历旧的生成器
+        if (fn(item)) { // 满足筛选条件就抛出
+            yield item
         }
     }
 }
 
 function* prime() {
-    let it = list()
+    let it = list() // 获取初始序列
 
     while (true) {
         const { value } = it.next()
         yield value
-        it = m_filter(filterFunc(value) , it)()// 构造新的生成器
+        it = m_filter(filterFunc(value), it)// 构造新的生成器
     }
 }
 
-for (const n of prime()) {
-    if (n < 10) {
+for (const n of prime()) { // 获取100以内的素数
+    if (n < 100) {
         console.log(n);
     } else {
         break
